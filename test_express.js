511775eda437d74a29a49db34029b2f342426276
oliviermanette <oliviermanette@flod.aero>
2018-02-01 17:01:21 +0100

    I love you too!

--- test_express.js
+++ test_express.js
@@ -2,7 +2,8 @@ var express = require('express');
 var formidable = require('formidable');
 var app = express();
 const sqlite3 = require('sqlite3').verbose();
-
+var sys = require('util');
+var fs = require('fs');
 
 app.get('/', function(req, res) {
     res.setHeader('Content-Type', 'text/html');
@@ -49,7 +50,7 @@ app.post('/upload', function(req, res)
     var form = new formidable.IncomingForm();
     var serialno = "unknown";
     var Fichiers = [];
-    form.uploadDir = "/home/eldecog/nodejs/upload";
+    form.uploadDir = "upload";
     form.keepExtensions = true;
 
     form.on('file', function (field, file) {
@@ -58,12 +59,16 @@ app.post('/upload', function(req, res)
 
     form.parse(req, function (err, fields, files) {
         res.writeHead(200, { 'content-type': 'text/plain' });
-        res.write('received upload : \n\n');
-        res.end(sys.inspect({ fields: fields, files: files }));
-        console.log({ fields: fields, files: files });
         serialno = fields['serialno'];
-        for (var i = 0, len = Fichiers.length; i < len; i++)
+        res.end('thank you for your upload');
+        //res.end(sys.inspect({ fields: fields, files: files }));
+        //console.log({ fields: fields, files: files });
+        serialno = fields['serialno'];
+        for (var i = 0, len = Fichiers.length; i < len; i++) {
             fs.rename(Fichiers[i], form.uploadDir + "/" + Date.now() + "_" + i + "_" + serialno + ".dat");
+            console.log('rename :' + Fichiers[i] + ' to ' + form.uploadDir + "/" + Date.now() + "_" + i + "_" + serialno + ".dat");
+        }
+            
     }
     );
 });
@@ -94,6 +99,121 @@ app.get('/watch/recording/:serialno', function (req, res) {
 
 });
 
+app.get('/watch/canistop/:serialno', function (req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('please wait ' + req.params.serialno);
+    console.log('pause requested by ' + req.params.serialno);
+    let db = new sqlite3.Database('../untitled/db/ptms.db', (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        //console.log('Connected to the PTMS database.');
+    });
+    let sql = "update Montres set status='canistop', LastActif='" + Date.now() +
+        "', IP='" + req.connection.remoteAddress +
+        "' where codeID='" + req.params.serialno + "'";
+    db.run(sql);
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+            console.log("ERROR : couldn't find " + req.params.serialno);
+        }
+        //console.log('Close the database connection.');
+    });
+});
+
+app.get('/watch/okstop/:serialno', function (req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('please wait ' + req.params.serialno);
+    console.log('pause requested by ' + req.params.serialno);
+    let db = new sqlite3.Database('../untitled/db/ptms.db', (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        //console.log('Connected to the PTMS database.');
+    });
+    let sql = "update Montres set status='stopped', LastActif='" + Date.now() +
+        "', IP='" + req.connection.remoteAddress +
+        "' where codeID='" + req.params.serialno + "'";
+    db.run(sql);
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+            console.log("ERROR : couldn't find " + req.params.serialno);
+        }
+        //console.log('Close the database connection.');
+    });
+});
+
+app.get('/watch/arrecording/:serialno', function (req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('please wait ' + req.params.serialno);
+    console.log('pause requested by ' + req.params.serialno);
+    let db = new sqlite3.Database('../untitled/db/ptms.db', (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        //console.log('Connected to the PTMS database.');
+    });
+    let sql = "update Montres set status='arrecording', LastActif='" + Date.now() +
+        "', IP='" + req.connection.remoteAddress +
+        "' where codeID='" + req.params.serialno + "'";
+    db.run(sql);
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+            console.log("ERROR : couldn't find " + req.params.serialno);
+        }
+        //console.log('Close the database connection.');
+    });
+});
+
+app.get('/watch/msgread/:serialno', function (req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('please wait ' + req.params.serialno);
+    console.log('pause requested by ' + req.params.serialno);
+    let db = new sqlite3.Database('../untitled/db/ptms.db', (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        //console.log('Connected to the PTMS database.');
+    });
+    let sql = "update Montres set status='msgread', LastActif='" + Date.now() +
+        "', IP='" + req.connection.remoteAddress +
+        "' where codeID='" + req.params.serialno + "'";
+    db.run(sql);
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+            console.log("ERROR : couldn't find " + req.params.serialno);
+        }
+        //console.log('Close the database connection.');
+    });
+});
+
+
+app.get('/watch/whatsup/:serialno', function (req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    let db = new sqlite3.Database('../untitled/db/ptms.db', (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        //console.log('Connected to the PTMS database.');
+    });
+    let sql = 'SELECT status FROM Montres WHERE codeID=?';
+    let param = req.params.serialno;
+    db.get(sql, [param] ,  (err, row) => {
+        if (err)
+            return console.error(err.message);
+
+       // res.end(row.status);        
+        return row
+            ? res.end(row.status)
+            : res.end('I love you too!');
+        
+    });
+    
+});
 // ... Tout le code de gestion des routes (app.get) se trouve au-dessus
 
 app.use(function (req, res, next) {
